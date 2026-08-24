6b02d861148aa337dfe5dd24dd8d55a1a795dc52
Malk Abdelhamed <marakuoo82@gmail.com>
2026-08-25 01:26:45 +0300

    Create NumberHistogram.cpp

---
+++ FunctionsAndLoops/NumberHistogram.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+   char c;cin>>c;
+   int n, number;cin>>n;
+   while(n--) {
+      cin>>number;
+      for(int i=1; i<=number; i++) {
+        cout<<c;
+      }cout<<'\n';
+   }
+
+
+
+
+
+
+
+
+
+}
