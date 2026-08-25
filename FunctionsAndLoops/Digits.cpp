1fd742948e076135469132f61440d00497fb4459
Malk Abdelhamed <marakuoo82@gmail.com>
2026-08-25 15:00:58 +0300

    Create Digits.cpp

---
+++ FunctionsAndLoops/Digits.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+int t; cin>>t;
+    long long num;
+while(t--){
+    cin>>num;
+    while(num!=0) {
+        cout<<num % 10 <<" ";
+        num/=10;
+    }
+
+    cout<<endl;
+}
+
+
+
+
+
+
+}
