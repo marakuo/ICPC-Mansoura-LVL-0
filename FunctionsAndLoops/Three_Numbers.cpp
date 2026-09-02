968a54b909fe40388433e3f131ee5f8cab99dc62
Malk Abdelhamed <marakuoo82@gmail.com>
2026-09-02 15:28:33 +0300

    Create Three_Numbers.cpp

---
+++ FunctionsAndLoops/Three_Numbers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  int k, s, ctr = 0; cin>>k>>s;
+  for(int i=0; i<=k; i++) {
+    for(int j=0; j<=k; j++) {
+      if (s -(i+j)>=0 && s-(i+j)<=k) ctr++;
+
+    }
+
+  }
+  cout<<ctr<<endl;
+
+
+
+}
